2da9e153c37254f434e2792c2cfdba370dfa92c8
Zapir <ghost.134@hotmail.com>
2021-10-23 09:53:05 +0300

    controllers

--- ApiGateway/ApiGateway/Startup.cs
+++ ApiGateway/ApiGateway/Startup.cs
@@ -18,6 +18,7 @@ namespace ApiGateway
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
             services.AddOcelot();
         }
 
@@ -33,10 +34,7 @@ namespace ApiGateway
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
 
             app.UseOcelot().Wait();
