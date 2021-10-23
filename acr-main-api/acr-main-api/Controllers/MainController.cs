aaca75b15edeb7c2270eeb72657107826bbf7047
Zapir <ghost.134@hotmail.com>
2021-10-23 22:50:41 +0300

    create project

---
+++ acr-main-api/acr-main-api/Controllers/MainController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace acr_main_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        [HttpGet("main-table")]
+        public IActionResult GetTable()
+        {
+
+        }
+    }
+}
---
+++ acr-main-api/acr-main-api/Models/Domain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace acr_main_api.Models
+{
+    public class Domain
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
