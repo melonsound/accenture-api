5ee5a857c30aeb641de82f00cb12e8ec7f7fa3f5
Zapir <ghost.134@hotmail.com>
2021-10-23 03:11:13 +0300

    feat: account model

---
+++ AuthService/AuthService/Models/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuthService.Models
+{
+    public class Account
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        [NotMapped]
+        public string Token { get; set; }
+    }
+}
