0fb4df1aa1fd3c42f01a6e6d9bd37d354c40e057
Zapir <ghost.134@hotmail.com>
2021-10-23 04:19:50 +0300

    feat: account context db

---
+++ AuthService/AuthService/Data/AccountContext.cs
@@ -0,0 +1,15 @@
+﻿using AuthService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Data
+{
+    public class AccountContext : DbContext
+    {
+        public AccountContext(DbContextOptions<AccountContext> options) : base(options)
+        {
+            
+        }
+        
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
