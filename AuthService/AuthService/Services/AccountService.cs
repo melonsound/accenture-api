606322c3f391d0a0d3b937b0660acc11aedbd4e4
Zapir <ghost.134@hotmail.com>
2021-10-23 04:18:22 +0300

    feat: account auth things

---
+++ AuthService/AuthService/Services/AccountService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using AuthService.Data;
+using AuthService.Helpers;
+using AuthService.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthService.Services
+{
+    public class AccountService
+    {
+        private readonly AccountContext _accountContext;
+        private readonly IOptions<AuthOptions> _options;
+
+        public AccountService(AccountContext accountContext, IOptions<AuthOptions> options)
+        {
+            _accountContext = accountContext;
+            _options = options;
+        }
+        public Account Authenticate(string username, string password)
+        {
+            var account = _accountContext.Accounts.SingleOrDefault(x => x.Username == username);
+
+            if (account == null || !BCrypt.Net.BCrypt.Verify(password, account.Password))
+                return null;
+
+            var authParams = _options.Value;
+
+            //generate jwt token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = authParams.GetSymmetricSecurityKey();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, account.Id.ToString()),
+                    new Claim(ClaimTypes.Role, account.Role)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            account.Token = tokenHandler.WriteToken(token);
+
+            return account;
+        }
+    }
+}
--- AuthService/AuthService/Startup.cs
+++ AuthService/AuthService/Startup.cs
@@ -1,12 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using AuthService.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.JsonWebTokens;
 
 namespace AuthService
 {
@@ -16,6 +21,25 @@ namespace AuthService
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AccountContext>();
+            var authKey = Encoding.ASCII.GetBytes(Config.AuthKey);
+            services.AddAuthentication(x => 
+                {
+                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer (x =>
+                {
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(authKey),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
