07eb5cb60764a92cc4216f15f17569643c01fe1f
Zapir <ghost.134@hotmail.com>
2021-10-23 23:12:22 +0300

    feat: data layer

---
+++ acr-main-api/acr-main-api/Data/MainContext.cs
@@ -0,0 +1,19 @@
+﻿using acr_main_api.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace acr_main_api.Data
+{
+    public class MainContext : DbContext
+    {
+        public MainContext(DbContextOptions<MainContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Domain> Domains { get; set; }
+    }
+}
